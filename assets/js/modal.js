b614fe08c984e78ec7a73bc73675161452013aa7
Omar Martins <omarmartinsabreu@gmail.com>
2024-02-13 23:50:48 -0300

    adciona estilo ao modal

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,28 +1,35 @@
-document.addEventListener('click', function(event) {
+document.addEventListener('click', function (event) {
     const pokemonItem = event.target.closest('.pokemon');
     if (pokemonItem) {
         const modal = document.getElementById('pokemonModal');
         const modalContent = modal.querySelector('.modal-content');
         const { number, name, types, photo } = pokemonItem.dataset;
 
+        const typesArray = types.split(',');
+        const typesList = typesArray.map(type => `<li class='type ${type.trim()}'>${type.trim()}</li>`).join('');
+
         modalContent.innerHTML = `
-            <span class="close">&times;</span>
-            <h2>${name}</h2>
-            <p>Number: #${number}</p>
-            <p>Type(s): ${types}</p>
-            <img src="${photo}" alt="${name}">
-        `;
+           <span class="close">&times;</span>
+           <content class='modalInfo'>
+                <div class='top'>            
+                    <h2 class='name'>${name}</h2>
+                    <p class='number'>#${number}</p>
+                </div>
 
+               <div class='detail'>
+               <ol class='types'>${typesList}</ul>
+                <img src="${photo}" alt="${name}">
+               </div>
+
+           </content>
+       `;
+        modalContent.classList.add(typesArray[0]);
         modal.style.display = 'block';
-    }
-});
 
-document.addEventListener('click', function(event) {
-    const closeButtons = document.querySelectorAll('.close');
-    closeButtons.forEach(function(button) {
-        if (event.target === button) {
-            const modal = document.getElementById('pokemonModal');
+        const closeButton = modal.querySelector('.close');
+        closeButton.addEventListener('click', function() {
+            modalContent.classList.remove(typesArray[0]);
             modal.style.display = 'none';
-        }
-    });
+        });
+    }
 });
